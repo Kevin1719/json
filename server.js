08c3906a8d4b0c1410631672d0144189559c5332
Kevin1719 <93263952+Kevin1719@users.noreply.github.com>
2022-07-16 17:58:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const path = require('path');
 const app = express();
 
 const paths = require('./db.json')
-
+app.all('/*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Length, Accept, Content-Type, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+    next();
+})
 app.get('/', (req, res) => {
     res.header("Content-Type", "application/json");
     res.send(JSON.stringify(paths))
